b7e8a38a112c065cac5338702ebc785700fd8b51
DESKTOP-RCS1AH7\Xeb <ygigamer@gmail.com>
2023-12-04 13:37:00 +0800

    UI fixes

--- AdmissionAceApplication/app/src/main/java/com/example/admissionaceapplication/Activities/ScoreActivity.java
+++ AdmissionAceApplication/app/src/main/java/com/example/admissionaceapplication/Activities/ScoreActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
+
 import com.example.admissionaceapplication.R;
 import com.example.admissionaceapplication.databinding.ActivityQuestionsBinding;
 import com.example.admissionaceapplication.databinding.ActivityScoreBinding;
@@ -22,7 +24,7 @@ public class ScoreActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-
+        TextView txt = findViewById(R.id.info);
         int totalScore = getIntent().getIntExtra("total",0);
         int correctAnsw = getIntent().getIntExtra("score", 0);
 
@@ -32,6 +34,16 @@ public class ScoreActivity extends AppCompatActivity {
         binding.rightAnsw.setText(String.valueOf(correctAnsw));
 
         binding.wrongAnsw.setText(String.valueOf(wrong));
+        if(correctAnsw >= 20)
+            txt.setText("Absolutely flawless! You've achieved a perfect score");
+        else if(correctAnsw > 15 && correctAnsw < 20)
+            txt.setText("Well done! You've grasped the majority of the questions");
+        else if(correctAnsw > 10 && correctAnsw < 15)
+            txt.setText("Good effort! There's room for improvement. Focus on the areas where you struggled.");
+        else if(correctAnsw < 10)
+            txt.setText("Room for growth. Identify specific areas that need attention.");
+        else
+            txt.setText("");
 
         binding.btnRetry.setOnClickListener(new View.OnClickListener() {
             @Override
--- AdmissionAceApplication/app/src/main/java/com/example/admissionaceapplication/Activities/SplashScreenActivity.java
+++ AdmissionAceApplication/app/src/main/java/com/example/admissionaceapplication/Activities/SplashScreenActivity.java
@@ -1,10 +1,15 @@
 package com.example.admissionaceapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.admissionaceapplication.MainActivity;
 import com.example.admissionaceapplication.R;
@@ -18,14 +23,21 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        ImageView bg = findViewById(R.id.imageView2);
+        ConstraintLayout cons = findViewById(R.id.textContainer);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash);
+        bg.startAnimation(animation);
+        cons.startAnimation(animation);
+        MediaPlayer ring = MediaPlayer.create(SplashScreenActivity.this, R.raw.splash); ring.start();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
-
+                finish();
             }
-        },2000);
+        },4000);
     }
 }
